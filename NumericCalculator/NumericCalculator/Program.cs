c63eaacead44b57ae889a3275946066446fdbccb
danielvinzia <danielvinzia@gmail.com>
2022-02-17 08:37:56 -0300

    variables en c sharp

---
+++ NumericCalculator/NumericCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number1;
+int number2;
+
+Console.WriteLine("Input the first number");
+number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input the Second number");
+number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = number1 * number2;
+Console.WriteLine("The Result Is: " + result);
+
---
+++ stringExample/stringExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace stringExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What`s your name? Please Write your full name");
+            String fullUserName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + fullUserName + ", Welcome to Dying");
+        }
+    }
+}

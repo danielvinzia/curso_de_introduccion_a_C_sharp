1314c4ab848ceadedfeedc7b716ed917258778f4
danielvinzia <danielvinzia@gmail.com>
2022-02-17 11:41:21 -0300

    Crear metodos propios en c sharp

---
+++ MyMethods/MyMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyMethods
+{
+    internal class Program
+    {   
+        public void IntegerAddition(int a, int b)
+        {
+            int addition = a + b;
+            Console.WriteLine("El resultado es " + addition);
+        }
+
+        public int IntegerMultiplication(int a, int b)
+        {
+            int Multiplication = a * b;
+            return Multiplication;
+        }
+
+        static void Main(string[] args)
+        {
+            Program MyProgram = new Program();
+            MyProgram.IntegerAddition(3, 6);
+
+            int result = MyProgram.IntegerMultiplication(12, 2);
+            Console.WriteLine("El resultado de la multiplicacion es " + result);
+
+        }
+    }
+}

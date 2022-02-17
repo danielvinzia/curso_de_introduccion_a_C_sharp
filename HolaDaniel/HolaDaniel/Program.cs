ca6b8b924ef6d0ebbd7e0d1712e5c9fb1455546f
danielvinzia <danielvinzia@gmail.com>
2022-02-17 09:23:52 -0300

    Manejo de strings en c sharp

---
+++ HolaDaniel/HolaDaniel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HolaDaniel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Daniel!");
+
+            int height = 178;
+            int age = 32;
+            string name = "Daniel Vinzia";
+            string information = "The information you resquested is: \nUser name: " + name 
+                + "\nUser age: " + age + "\nUser height: " + height; 
+            Console.WriteLine(information);
+        }
+    }
+}

298bb103827f3faccf0686b7a99f2df3520131e8
danielvinzia <danielvinzia@gmail.com>
2022-02-17 10:05:42 -0300

    operadores racionales en c sharp

--- LogicalOperatorExample/LogicalOperatorExample/Program.cs
+++ LogicalOperatorExample/LogicalOperatorExample/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
+// && , || , !
+/*
 bool value1 = true;
 bool value2 = true;
 bool value3 = false;
 
-// && , || , !
+
 
 bool result = value1 && value2 && value3;
 Console.WriteLine("el resultado del AND logico es: " + result);
@@ -15,3 +16,36 @@ Console.WriteLine("el resultado del OR logico es: " + result2);
 
 bool result3 = !value1; 
 Console.WriteLine("el resultado del NOT logico es: " + result3);
+*/
+
+int value1 = 18;
+int value2 = 18;
+int value3 = -18;
+
+bool result0 = value1 == value2;
+Console.WriteLine("el resultado de value1 == value2 is: " + result0);
+
+bool result1 = value1 != value2;
+Console.WriteLine("el resultado de value1 != value2 is: " + result1);
+
+bool result2 = value1 > value2;
+Console.WriteLine("el resultado de value1 > value2 is: " + result2);
+
+bool result3 = value1 < value2;
+Console.WriteLine("el resultado de value1 < value2 is: " + result3);
+
+bool result4 = value1 >= value2; 
+Console.WriteLine("el resultado de value1 >= value2 is: " + result4);
+
+bool result5 = value1 <= value2;
+Console.WriteLine("el resultado de value1 <= value2 is: " + result5);
+
+
+/*
+==  igual a
+!=  no igual a
+> mayor que
+< menor que
+>= mayor que o igual que
+<= menor que o igual que
+*/

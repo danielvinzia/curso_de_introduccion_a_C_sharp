6f38455789e66fbcd7a310f5025f921689e3558e
danielvinzia <danielvinzia@gmail.com>
2022-02-17 10:33:29 -0300

    Arreglos en c sharp

---
+++ ArrayDemoProyect/ArrayDemoProyect/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Bienvenido a la Cafeteria!");
+
+string[] coffeeTypes;
+float[] coffeePrices;
+
+coffeeTypes = new string[] { "Expresso", "Largo", "Filtrado", "Latte" };
+coffeePrices = new float[] {  1.2f,        1.5f,      5f,       5.5f};
+
+coffeeTypes[1] = "Lungo";
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine(coffeeTypes[i] + " coffee $" + coffeePrices[i]);
+}

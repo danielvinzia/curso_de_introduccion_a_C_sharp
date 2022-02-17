3daca6aa7331f7ca69deede19bb0ec48bab2aec4
danielvinzia <danielvinzia@gmail.com>
2022-02-17 09:51:46 -0300

    operadores logicos en c sharp

---
+++ LogicalOperatorExample/LogicalOperatorExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool value1 = true;
+bool value2 = true;
+bool value3 = false;
+
+// && , || , !
+
+bool result = value1 && value2 && value3;
+Console.WriteLine("el resultado del AND logico es: " + result);
+
+bool result2 = value1 || value2 || value3;
+Console.WriteLine("el resultado del OR logico es: " + result2);
+
+bool result3 = !value1; 
+Console.WriteLine("el resultado del NOT logico es: " + result3);

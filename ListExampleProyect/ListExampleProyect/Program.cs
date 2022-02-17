54f0e9c72165da3d8d3da0bd939cccdadb10533e
danielvinzia <danielvinzia@gmail.com>
2022-02-17 10:50:12 -0300

    listas en c sharp

---
+++ ListExampleProyect/ListExampleProyect/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<string> tacoShoppingList = new List<string>();
+
+tacoShoppingList.Add("cinco tacos de suadero");
+tacoShoppingList.Add("cuatro tacos de tripa");
+tacoShoppingList.Add("cinco tacos de pastor");
+tacoShoppingList.Add("cuatro coca cola");
+
+for(int i = 0; i < tacoShoppingList.Count; i++)
+{
+    Console.WriteLine(tacoShoppingList[i]);
+}
+
+//tacoShoppingList.Remove("cinco tacos de suadero");
+tacoShoppingList.RemoveAt(0);
+
+for (int i = 0; i < tacoShoppingList.Count; i++)
+{
+    Console.WriteLine(tacoShoppingList[i]);
+}

0ca61812b4cdf5a203e08bcf2c1e9e68b0827d15
danielvinzia <danielvinzia@gmail.com>
2022-02-17 09:38:40 -0300

    trabajando con numeros y operadores aritmeticos en c sharp

---
+++ SquareArea/SquareArea/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // este programa calcula el area de un rectangulo
+            // rectangle area calculator
+
+            // valores del rectangulo
+            // rectangle values
+            float sideA = 10.0f ;
+            int sideB = 1 ;
+            
+            // la formula del area de un rectangulo es A*B
+            // rectangle area formula is a*b
+            float area = sideA * sideB;
+
+            // + - / * %
+            // ++ -- 
+
+            Console.WriteLine("El area del rectangulo es: " + area);
+        }
+    }
+}

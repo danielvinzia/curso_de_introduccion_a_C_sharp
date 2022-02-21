2f8a2172a88256330d03c8387c3108508870fed8
danielvinzia <danielvinzia@gmail.com>
2022-02-21 10:38:01 -0300

    clase de ciclo for concluida

---
+++ ForCicle/ForCicle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForCicle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 50 ; i=i+10)
+            {
+                Console.WriteLine("Hello World! #{0}", i);
+            }            
+        }
+    }
+}
